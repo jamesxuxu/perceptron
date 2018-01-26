2cfabf23e67e5332d207b0f0e21638f87680b3a8
Xin Bian <xinbian@users.noreply.github.com>
2018-01-26 14:49:03 -0500

    one loop for all

--- Bian_perceptron.py
+++ Bian_perceptron.py
@@ -16,12 +16,12 @@ argu = parser.parse_args()
 
 nstep = argu.nstep
 noDev = argu.noDev
-
+nstep = 10
 #define read data function
 def readData(dataset):
     y = []
     x = []
-    path = ['/u/cs246/data/adult/a7a' , dataset]
+    path = ['./adult/a7a' , dataset]
     filepath = '.'.join(path)
     with open(filepath) as f:
         trainData = csv.reader(f, delimiter=' ')
@@ -32,10 +32,10 @@ def readData(dataset):
             temp[-1] = 1
             for element in row[1:]:
                 if len(element) != 0:
-  		    #things are confusing here
-                    #the feature index can be start from 1 or 0.
+                    #things are confusing here
+                    #the feature index can start from 1 or 0.
                     #if considering 1 to 123, I should use markLoc = int(element.split(':')[0]) - 1
-                    #to be consistent with the example, I consider it 0 to 122: 
+                    #to be consistent with the example, I consider it 0 to 122:
                     markLoc = int(element.split(':')[0])
                     temp[markLoc] = 1
             
@@ -46,48 +46,41 @@ def readData(dataset):
 xTrain, yTrain = readData('train')
 xTest, yTest = readData('test')
 
+if noDev == False:
+    xDev, yDev = readData('dev')
+
 #function used to train model
-def model(nstep):
-    w = np.zeros((124,1))
-    for _ in range(nstep):
-        for index, xn in enumerate(xTrain):
-            t = np.sign(np.dot(np.transpose(w), xn))        
-            if t!= yTrain[index]:
-                w = w + yTrain[index]*xn 
+def model(wPre):
+    w = wPre
+    for index, xn in enumerate(xTrain):
+        t = np.sign(np.dot(np.transpose(w), xn))        
+        if t!= yTrain[index]:
+            w = w + yTrain[index]*xn 
     return w
     
 
     
-##test result on test dataset
-right = 0
-w = model(nstep)
-for index, xn in enumerate(xTest):
-    t = np.sign(np.dot(np.transpose(w), xn))
-    if t*yTest[index] > 0:
-        right += 1
-accuracy = right/float(len(xTest))
+##test result on test dataset and dev set
+accDev = []
+w = np.zeros((124,1))
+for i in range(nstep):
+    w = model(w)
+    if noDev == False:
+        right = 0
+        for index, xn in enumerate(xDev):
+            t = np.sign(np.dot(np.transpose(w), xn))
+            if t*yDev[index] > 0:
+                right += 1
+        accDev.append(right/float(len(xDev)))
+    if i == nstep-1:
+        right = 0
+        for index, xn in enumerate(xTest):
+            t = np.sign(np.dot(np.transpose(w), xn))
+            if t*yTest[index] > 0:
+                right += 1
+        accuracy = right/float(len(xTest))
 
     
 print('Test accuracy:', accuracy)
 print('Feature weights (bias last):', ' '.join(map(str, np.transpose(w)[0])))
-    
-
-##dev to adjust hyperparameter
-acc = []
-if noDev == False:
-    xDev, yDev = readData('dev')
-    for i in range(nstep):
-        right = 0
-        wDev = model(i+1)
-        for index, xn in enumerate(xDev):
-            t = np.sign(np.dot(np.transpose(wDev), xn))
-            if t*yDev[index] > 0:
-                right += 1
-        acc.append(right/float(len(xDev)))
-    print('dev accuracy vs. iteration:', acc)
-   # plt.plot(acc)
-   # plt.show()
-    
-    
-    
-            
+print('dev set accuracy vs. iterations:', accDev)
